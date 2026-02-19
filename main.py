35c196ede365045b09c529694eda5e63938f347a
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-19 13:49:00 +0700

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
@@ -20,7 +21,24 @@ layout = QVBoxLayout(central_widget)
 
 canvas = MplCanvas()
 layout.addWidget(canvas)
+def f(x, y):
+    return x - y
 
+density = 20
+x = np.linspace(-5, 5, density)
+y = np.linspace(-5, 5, density)
+
+X, Y = np.meshgrid(x, y)
+
+DY = f(X, Y)
+DX = np.ones_like(DY)
+
+magnitude = np.sqrt(DX**2 + DY**2)
+DX = DX / magnitude
+DY = DY / magnitude
+
+canvas.ax.quiver(X, Y, DX, DY)
+canvas.draw()
 window.setCentralWidget(central_widget)
 window.show()
 
